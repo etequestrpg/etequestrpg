0489e22a90590e5811283b545ee3bc86ef4d2e27
etequestrpg <piorparty312@gmail.com>
2025-03-13 20:38:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // ---------------------------------------------
 // Abrir a secção de "informações" quando clicar
 // ---------------------------------------------
-// Get all buttons with the class 'learn-more-btn'
-const buttons = document.querySelectorAll('.learn-more-btn');
-// Loop through each button and add an event listener
+const buttons = document.querySelectorAll('.info');
 buttons.forEach(button => {
 	button.addEventListener('click', () => {
-		// Toggle the 'active' class on the parent .campaign div
 		const campaign = button.closest('.campaign');
 		campaign.classList.toggle('active');
 	});
